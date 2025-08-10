1320bf71e7441bec12154a41b923647ddd65cae0
balasri <n210342@rguktn.ac.in>
2025-08-10 22:37:28 +0530

    day 9 of 15-days-coding-challenge

---
+++ day_09/question_3.py
@@ -0,0 +1,14 @@
+class Solution:
+    def subarraySum(self, nums: list[int], k: int) -> int:
+        count = 0
+        prefix_sum = 0
+        sum_freq = {0: 1} 
+        
+        for num in nums:
+            prefix_sum += num
+
+            if (prefix_sum - k) in sum_freq:
+                count += sum_freq[prefix_sum - k]
+            sum_freq[prefix_sum] = sum_freq.get(prefix_sum, 0) + 1
+        
+        return count
