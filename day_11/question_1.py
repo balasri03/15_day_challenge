fc1775501bc9a4801a8b1fe45879709cebd86285
balasri <n210342@rguktn.ac.in>
2025-08-12 23:54:42 +0530

    day 11 of 15-days-coding-challenge

---
+++ day_11/question_1.py
@@ -0,0 +1,6 @@
+def lowestCommonAncestor(root, p, q):
+    if not root or root == p or root == q:
+        return root
+    left = lowestCommonAncestor(root.left, p, q)
+    right = lowestCommonAncestor(root.right, p, q)
+    return root if left and right else left or right
